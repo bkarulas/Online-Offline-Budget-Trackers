aa234e3d03ded2854fa787a44657d3daeefe10b4
Brad <bkarulas@outlook.com>
2020-02-12 22:09:09 -0500

    app is working offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,34 +1,55 @@
-var CACHE_NAME = 'my-site-cache-v1';
-
-var urlsToCache = [
-  '/',
-  '/styles.css',
-  '/index.js'
+const FILES_TO_CACHE = [
+  "/public/db.js",
+  "/public/index.html",
+  "/public/index.js",
+  "/public/style.css",
+  "/routes/api.js",
+  "/models/transaction.js",
+  "/server.js",
 ];
 
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
+const PRE_CACHE = "pre_cache-v1";
+const RUNTIME = "runtime";
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(PRE_CACHE)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
   );
 });
 
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          return caches.match("/index.html");
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", event => {
+  const currentCaches = [PRE_CACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
         }
-      });
-    })
-  );
-});
 
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
